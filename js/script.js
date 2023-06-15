88a26d4ca737114c4adfb86ba51422441b498919
iankit-tanwar <tankitofficial01.gmail.com>
2023-06-15 10:43:44 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+//1 Function definatation is the one time process 
+function getmyname(){  // this function name is not follow camelCase standard
+
+   console.log("Ankit");
+}
+
+function getNyName(){ //this function name is follow as per camelCase standard
+
+    console.log("Tanwar");
+
+}
+
+//2 function calling many time process
+// I can call the function on the top it still working
+getmyname();
+
+getNyName();
+
+
+
+
+
+
---
+++ js/script2.js
@@ -0,0 +1,19 @@
+
+
+// 1 function defination one time process 
+
+function addTwoNumbers( n1 , n2){
+
+    //every function return something
+    return n1 + n2 ;
+
+}
+
+//2 function calling many time process
+
+//Associativity Operator perform execution 
+//Right  to Left
+
+ var res = addTwoNumbers(10,90);
+
+ console.log(res);
